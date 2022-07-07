281e691b570748ab17978b7e3f69622ef2b2190c
Mikhael Julian Ramardi <61940351+Mikhaeljulian@users.noreply.github.com>
2022-07-07 21:52:12 +0700

    Penambahan Urutan Materi

---
+++ 36. StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10, 11, 12, 13, 14, 15]));
+var_dump(explode(" ", "Mikhael Julian Ramardi"));
+var_dump(strtolower("MIKHAEL JULIAN RAMARDI"));
+var_dump(strtoupper("mikhael julian ramardi"));
+var_dump(trim("        mikhael      julian       "));
+var_dump(substr("Mikhael Julian Ramardi", 0, 7));
