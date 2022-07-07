13d46cbed9877d8664d9b7ffe3a77fcd9143ec51
Mikhael Julian Ramardi <61940351+Mikhaeljulian@users.noreply.github.com>
2022-07-07 21:51:37 +0700

    Penambahan Urutan Materi

---
+++ 35. Komentar.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Ini adalah function untuk menampilkan
+tulisan halo semua
+ */
+function sayHello()
+{
+    // ini komentar satu baris
+    echo "Halo Semua" . PHP_EOL;
+
+    /*
+    echo "Ini Komentar Dua Baris".
+    PHP_EOL;
+    */
+}
+
+sayHello();
